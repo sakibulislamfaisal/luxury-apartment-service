3fcf8127477d7307e5a6c28de8d4c0fd9454ac8c
sakibulislamfaisal <foyshalrahmanseu@gmail.com>
2021-06-16 00:17:42 +0600

    responsive react navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../../assets/Image/heading.PNG";
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
+  let style = {
+    textDecoration: "none",
+    padding: "15px",
+    fontSize: "17px",
+    fontWeight: "bold",
+    color: "black",
+  };
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          EPIC
+          <img src={logo} alt="logo" />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -24,18 +31,25 @@ function Navbar() {
           </li>
 
           <li className="nav-item">
-            <Link to="/products" className="nav-links">
-              Products
+            <Link to="/about" className="nav-links">
+              About us
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact-us" className="nav-links">
-              Contact Us
+            <Link to="/services" className="nav-links">
+              Services
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/sign-up" className="nav-links">
-              Sign Up
+            <Link to="/admin" className="nav-links">
+              Admin
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/sign-up" className="nav-links" style={{ style }}>
+              <button className="hover:bg-pink-700 text-white  py-1 -mt-1 px-6 rounded-sm ">
+                Login
+              </button>
             </Link>
           </li>
         </ul>
