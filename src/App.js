12edc788216ba7ccddb2938b94b9439ffdb7b500
sakibulislamfaisal <foyshalrahmanseu@gmail.com>
2021-06-16 10:41:20 +0600

    responsive menu fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 function App() {
-  return <>
-        <Router>
-          <Navbar></Navbar>
-             <Switch>
-                   <Route exact path="/">
-                        <Banner></Banner>
-                   </Route>
-             </Switch>
-        </Router>
-   </>;
+  return (
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Banner></Banner>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
